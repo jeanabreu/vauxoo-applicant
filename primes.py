3aa631c2ab383c08b9443fabd04ecd5cd125dc7e
Jean Abreu <abreustudio@gmail.com>
2017-03-26 14:55:47 -0400

    Resultado a asignación "py1"

--- primes.py
+++ primes.py
@@ -1,16 +1,31 @@
 """
-Your module documentation here
+Resolucion a problema py1:
+Codigo que reciba como entrada un número entero y que de salida regrese un True o False.
 """
 
 
-class PrimeClass(object):
-    """
-    Your class documentation here
-    """
+class prime_class:
 
-    def is_prime(self, num_int):
-        """
-        Your method documentation here
-        """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+    """
+    Se crea una variable que captura el numero ingresado    
+    """
+    input_n = raw_input("Ingrese un numero: ")
+    number = int(input_n)
+
+    def is_prime(self):
+        counter = 0
+        # Se inicializa un bucle para calcular el numero dado.
+        for i in range(1, self.number + 1):
+            if (self.number % i) == 0:
+                counter = counter + 1
+            if counter >= 3:
+                break
+        # Dado el resultado, mediante condiciones se arroja SI es primo o NO
+        val_bool = counter
+        if val_bool == 2:
+            print(val_bool == 2)
+        else:
+            print(val_bool == 0)
+# Se muestra el resultado mediante el metodo "is_prime"
+obj = prime_class()
+obj.is_prime()
