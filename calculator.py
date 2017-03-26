33e2393d73b3c9a44d5781010e1bc5a285304bbc
Jean Abreu <abreustudio@gmail.com>
2017-03-26 15:04:19 -0400

    Resolución a asignación "py2"

--- calculator.py
+++ calculator.py
@@ -1,16 +1,26 @@
 """
-Your module documentation here
+Resolucion a problema py2:
+Que reciba como entrada una lista de números números y 
+que de salida regrese la suma de los mismos.
 """
 
 
-class CalculatorClass(object):
-    """
-    Your class documentation here
-    """
+class calculator_class:
+        # Mediente una lista, almacenaremos los numeros a sumar en el metodo
+    list = []
+    sum_number = 0
 
-    def sum(self, num_list):
-        """
-        Your method documentation here
-        """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+    def sum(self):
+        # El valor ingresado en La Variable "x" por el usuario, determinara el
+        # tamano del arreglo
+        x = int(raw_input("Cantidad de numeros a Sumar:"))
+        for i in range(x):
+            # El usuario ingresara los numeros a sumar
+            self.list.append(int(raw_input("Registre el Numero:")))
+        for i in self.list:
+            self.sum_number += i
+        print "\n Total de la Suma:", self.sum_number
+
+# Se muestra el resultado mediante el metodo "sum"
+obj = calculator_class()
+obj.sum()
