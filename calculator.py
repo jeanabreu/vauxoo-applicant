11c014b036a98516614fce74fc5be4095d5920c3
Jean Abreu <abreustudio@gmail.com>
2017-03-27 19:47:53 -0400

    Respuesta a problema "py2"

--- calculator.py
+++ calculator.py
@@ -1,26 +1,26 @@
 """
-Resolucion a problema py2:
-Que reciba como entrada una lista de números números y 
-que de salida regrese la suma de los mismos.
+Resolution to problem py2:
+This script will receive as input a list of numbers and
+That output returns the sum of them.
 """
 
 
 class calculator_class:
-    # Mediente una lista, almacenaremos los numeros a sumar en el metodo
+    # Using a list, we will save the numbers to be added in the method
     list = []
     sum_number = 0
 
     def sum(self):
-        # El valor ingresado en La Variable "x" por el usuario, determinara el
-        # tamano del arreglo
-        x = int(raw_input("Cantidad de numeros a Sumar:"))
+        # The value entered in Variable "x" by the user, will be the size of
+        # the list.
+        x = int(raw_input("Size of the List: "))
         for i in range(x):
-            # El usuario ingresara los numeros a sumar
-            self.list.append(int(raw_input("Registre el Numero:")))
+            # The user will type the numbers to sum
+            self.list.append(int(raw_input("Type the number: ")))
         for i in self.list:
             self.sum_number += i
-        print "\n Total de la Suma:", self.sum_number
+        print "\n Sum Total:", self.sum_number
 
-# Se muestra el resultado mediante el metodo "sum"
+# The "sum" method will show the result
 obj = calculator_class()
 obj.sum()
