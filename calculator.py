f5e6acd878970ba745c7eba4f8f09611711b3ed9
Jean Abreu <abreustudio@gmail.com>
2017-03-28 08:39:43 -0400

    py1 and py2

--- calculator.py
+++ calculator.py
@@ -8,24 +8,19 @@ class CalculatorClass(object):
     Using a list, we will save the numbers to be added in the method
     """
     list = []
-    sum_number = 0
-
-    def sum(self):
+    
+    def sum(self, num_list ):
         """
         Method to calculate the sum of a list
         """
         # The value entered in Variable "x" by the user, will be the size of
         # the list.
+        num_list = 0
         list_input = int(raw_input("Size of the List: "))
         for nbr in range(list_input):
             # The user will type the numbers to this sum
             self.list.append(int(raw_input("Type the number: ")))
         for nbr in self.list:
-            self.sum_number += nbr
-        print "\n Sum Total:", self.sum_number
-
-
-# The "sum" method will show the result
-# pylint: disable=C0103
-calc = CalculatorClass()
-calc.sum()
+            self.num_list += nbr
+        return self.num_list
+        
--- primes.py
+++ primes.py
@@ -9,13 +9,13 @@ class PrimeClass(object):
     We created a variable to capture the integer number
     """
     input_n = raw_input("Type a Number: ")
-    num_int = int(input_n)
-
-    def is_prime(self):
+    
+    def is_prime(self, num_int):
         """
         Method to calculate if a number is prime or NOT
         """
         # A loop is initialized to calculate the given number.
+        num_int = int(input_n)
         counter = 0
         for nbr in range(1, self.num_int + 1):
             if (self.num_int % nbr) == 0:
@@ -26,12 +26,7 @@ class PrimeClass(object):
         # comparison with val_bool variable
         val_bool = counter
         if val_bool == 2:
-            print val_bool == 2
+            return True
         else:
-            print val_bool == 0
-
-
-# The "is_prime" method will show the result
-# pylint: disable=C0103
-prime = PrimeClass()
-prime.is_prime()
+            return False
+            
