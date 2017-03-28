8cffe216ddc3c69af893eb9456d483220626307b
Jean Abreu <abreustudio@gmail.com>
2017-03-27 20:04:33 -0400

    Respuesta al problema "py1"

--- primes.py
+++ primes.py
@@ -1,31 +1,30 @@
 """
-Resolucion a problema py1:
-Codigo que reciba como entrada un número entero y que de salida regrese un True o False.
+Resolution to problem py1:
+This script will receive an integer and the output will 
+return if it is prime or NOT with a True or False.
 """
 
 
 class prime_class:
-
-    """
-    Se crea una variable que captura el numero ingresado    
-    """
-    input_n = raw_input("Ingrese un numero: ")
+    # We created a variable to capture the integer number
+    input_n = raw_input("Type a Number: ")
     number = int(input_n)
 
     def is_prime(self):
         counter = 0
-        # Se inicializa un bucle para calcular el numero dado.
+        # A loop is initialized to calculate the given number.
         for i in range(1, self.number + 1):
             if (self.number % i) == 0:
                 counter = counter + 1
             if counter >= 3:
                 break
-        # Dado el resultado, mediante condiciones se arroja SI es primo o NO
+        # With the results, we show if the number is prime or NOT in a
+        # comparison with val_bool variable
         val_bool = counter
         if val_bool == 2:
             print(val_bool == 2)
         else:
             print(val_bool == 0)
-# Se muestra el resultado mediante el metodo "is_prime"
+# The "is_prime" method will show the result
 obj = prime_class()
 obj.is_prime()
