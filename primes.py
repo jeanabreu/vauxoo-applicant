6ffe4599c3451108484a695a4ae72512b7ef3488
Jean Abreu <abreustudio@gmail.com>
2017-03-27 23:39:19 -0400

    py1

--- primes.py
+++ primes.py
@@ -12,14 +12,14 @@ class PrimeClass(object):
 
     def is_prime(self):
         counter = 0
-        # A loop is initialized to calculate the given number.
-        for i in range(1, self.num_int + 1):
-            if (self.num_int % i) == 0:
+        """ A loop is initialized to calculate the given number."""
+        for nbr in range(1, self.num_int + 1):
+            if (self.num_int % nbr) == 0:
                 counter = counter + 1
             if counter >= 3:
                 break
-        # With the results, we show if the number is prime or NOT in a
-        # comparison with val_bool variable
+        """ With the results, we show if the number is prime or NOT in a"""
+        """ comparison with val_bool variable """
         val_bool = counter
         if val_bool == 2:
             print val_bool == 2
@@ -27,6 +27,6 @@ class PrimeClass(object):
             print val_bool == 0
 
 
-# The "is_prime" method will show the result
-obj = PrimeClass()
-obj.is_prime()
+""" The "is_prime" method will show the result"""
+result_prime = PrimeClass()
+result_prime.is_prime()
